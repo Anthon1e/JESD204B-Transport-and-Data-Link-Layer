f0a37e19a3fb34db01c3709d9a32f44df3fb0125
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-26 16:55:55 +0700

    Post-synthesis works

--- Receiver/jesd204b_tpl_rx.v
+++ Receiver/jesd204b_tpl_rx.v
@@ -22,20 +22,22 @@
 
 module jesd204b_tpl_rx #(
 	/* Parameters declaration */
-	parameter LANES = 4,		    // Number of lanes in the link
-	parameter CONVERTERS = 8,	  // Number of converters
+	parameter LANES = 4,		// Number of lanes in the link
+	parameter CONVERTERS = 8,	// Number of converters
 	parameter RESOLUTION = 11,	// Converter resolution
-	parameter CONTROL = 2, 		  // Number of control bit
+	parameter CONTROL = 2, 		// Number of control bit
 	parameter SAMPLE_SIZE = 16,	// Number of bits per sample
-	parameter SAMPLES = 1 		  // Number of samples per frame
+	parameter SAMPLES = 1 		// Number of samples per frame
 	) (
 	/* Input, output declaration 
-		Input: Lane 0 with 4 octets will be tx_dataout[31:0]
+		Input: Lane 0 with 4 octets will be rx_dataout[31:0]
 		        starting at Octet 0 [31:24] -> Octet 4 [7:0]          
-        Output: Converter 0 with resolution 11 will be tx_datain[10:0] */
+        Output: Converter 0 with resolution 11 will be rx_datain[10:0] */
     input clk,
-	input [SAMPLES*SAMPLE_SIZE*(CONVERTERS+(LANES-CONVERTERS%LANES)*|(CONVERTERS%LANES))-1:0] tx_datain,
-    output reg [SAMPLES*CONVERTERS*RESOLUTION-1:0] tx_dataout
+    input reset,
+    input en,
+	input [SAMPLES*SAMPLE_SIZE*(CONVERTERS+(LANES-CONVERTERS%LANES)*|(CONVERTERS%LANES))-1:0] rx_datain,
+    output reg [SAMPLES*CONVERTERS*RESOLUTION-1:0] rx_dataout
 	);
 	
 	/* Parameters calculation */
@@ -44,18 +46,23 @@ module jesd204b_tpl_rx #(
                         (LANES-CONVERTERS%LANES)*|(CONVERTERS%LANES)))/(8*LANES) ;  // Number of octets per frame per lane
     
     /* Actual mapping of the transport layer */ 
-    integer i, j, k;   // represent lane index, octet index, converter index
+    integer i, j;
+    (* KEEP = "TRUE" *) reg [3:0] k;   // represent lane index, octet index, converter index
     always @(posedge clk) begin 
-       k = 0;
-        // Looping for each lane
-        for (i = 0; i < LANES; i = i+1) begin
-            // Looping for every 2 octets 
-            for (j = OCTETS; j > 0; j = j-2) begin
-                if (k < CONVERTERS) begin
-                    tx_dataout[k*RESOLUTION +: RESOLUTION] = tx_datain[i*8*OCTETS+(j-2)*8+CONTROL+TAILS +: RESOLUTION];
-                    // Next converter index
-                    k = k + 1;    
-                end 
+        if (reset) begin 
+            rx_dataout = 0;
+        end else if (en) begin 
+            k = 0;
+            // Looping for each lane
+            for (i = 0; i < LANES; i = i+1) begin
+                // Looping for every 2 octets 
+                for (j = OCTETS; j > 0; j = j-2) begin
+                    if (k < CONVERTERS) begin
+                        rx_dataout[k*RESOLUTION +: RESOLUTION] = rx_datain[i*8*OCTETS+(j-2)*8+CONTROL+TAILS +: RESOLUTION];
+                        // Next converter index
+                        k = k + 1;    
+                    end 
+                end
             end
         end
     end
