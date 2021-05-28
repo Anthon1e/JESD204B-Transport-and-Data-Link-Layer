666d38c0ef650271d77c1f163f2b159b7748c4ab
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-28 14:18:37 +0700

    Add alt scrambler to avoid data loss at start up

--- Transmitter/jesd204b_scrambler.v
+++ Transmitter/jesd204b_scrambler.v
@@ -36,30 +36,30 @@ module jesd204b_scrambler #(
     reg [14:0] state = 'h7f80;
     
     /* Looping through the input data bit, starting from MSB */
-    integer i;
+    integer i, j;
     always @(*) begin
         if (reset) begin 
             out = 'h0;
             storage = 'h7f80;
         end else begin
             storage = state;
-            if (in === 'hx) begin
-            end else begin
-                if (en) begin 
-                    for (i = DATA_WIDTH; i > 0; i = i - 1) begin 
-                        out[i-1] = in[i-1] ^ storage[14] ^ storage[13];
-                        // Replace LSB of storage with out, push the
-                        //  remaining to the right. Value of bit 0 is gone
-                        storage = {storage[13:0], out[i-1]}; 
-                    end
+            j = 0; 
+            if (en) begin 
+                for (i = DATA_WIDTH; i > 0; i = i - 1) begin 
+                    if (j < 16) out[i-1] = in[i-1];
+                    else out[i-1] = in[i-1] ^ storage[14] ^ storage[13];
+                    // Replace LSB of storage with out, push the
+                    //  remaining to the right. Value of bit 0 is gone
+                    storage = {storage[13:0], out[i-1]}; 
+                    j = j + 1;
                 end
-                else out = in;
             end
+            else out = in;
         end
     end
     
     always @(posedge clk) begin 
-        if (reset)  state <= 'h7f80;
-        else        state <= storage;
+        if (reset)  state = 'h7f80;
+        else        state = storage;
     end
 endmodule
