de4a31918cd440dc09a32e36baecc8e88abd0cf8
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-28 14:20:45 +0700

    Add alt descrambler to avoid data loss at start up

--- Receiver/jesd204b_descrambler.v
+++ Receiver/jesd204b_descrambler.v
@@ -22,7 +22,7 @@
 
 module jesd204b_descrambler #(
 	/* Parameters declaration */
-    parameter DATA_WIDTH = 128
+    parameter DATA_WIDTH = 64
     ) (
     input clk,
     input reset,
@@ -31,24 +31,27 @@ module jesd204b_descrambler #(
     output reg [DATA_WIDTH-1:0] out
     );
     
-    // In descrambler, no need for preset
+    // 1 for 8 highest-storage elements, rest are 0
     reg [14:0] storage;
     
     /* Looping through the input data bit, starting from MSB */
-    integer i;
+    integer i, j;
     always @(*) begin
         if (reset) begin 
-            out <= 0;
+            out = 0;
         end else begin
+            j = 0;
             if (en) begin
                 for (i = DATA_WIDTH; i > 0; i = i - 1) begin 
-                    out[i-1] = in[i-1] ^ storage[14] ^ storage[13];
+                    if (j < 16) out[i-1] = in[i-1];
+                    else out[i-1] = in[i-1] ^ storage[14] ^ storage[13];
                     // Replace LSB of storage with in, push the
                     //  remaining to the right. Value of bit 0 is gone
                     storage = {storage[13:0], in[i-1]}; 
+                    j = j + 1;
                 end
             end
-            else out <= in;
+            else out = in;
         end
     end
 endmodule
