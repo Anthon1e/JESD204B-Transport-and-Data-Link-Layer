1947e3df2ec848a6876804d5416f26240114ab3f
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-28 14:19:36 +0700

    Update jesd204b_tx_tb.v

--- Transmitter/jesd204b_tx_tb.v
+++ Transmitter/jesd204b_tx_tb.v
@@ -23,11 +23,11 @@
 module jesd204b_tx_tb #(
     parameter DATA_WIDTH = 64,
     parameter LANES = 4,           // Number of lanes in the link
-	parameter CONVERTERS = 4,      // Number of converters
-	parameter RESOLUTION = 11,     // Converter resolution
-	parameter CONTROL = 2,         // Number of control bit
-	parameter SAMPLE_SIZE = 16,    // Number of bits per sample
-	parameter SAMPLES = 1          // Number of samples per frame
+    parameter CONVERTERS = 4,      // Number of converters
+    parameter RESOLUTION = 11,     // Converter resolution
+    parameter CONTROL = 2,         // Number of control bit
+    parameter SAMPLE_SIZE = 16,    // Number of bits per sample
+    parameter SAMPLES = 1          // Number of samples per frame
 	);
     
     reg clock, reset;
@@ -51,20 +51,20 @@ module jesd204b_tx_tb #(
     
     initial begin
         // Set up for the rise of clock every 2 seconds
-        clock = 1'b1;
+        clock <= 1'b1;
         #1;
         forever begin
-            clock = 1'b0;
+            clock <= 1'b0;
             #1;
-            clock = 1'b1;
+            clock <= 1'b1;
             #1;
         end
     end
     
     initial begin
         #120;
-        reset <= 1;
         tx_datain <= 'h0;
+        reset <= 1;
         #2; 
         tx_datain <= 'h12345678_abc;
         reset <= 0;
